b68ca636dae38eba76c15e19f0bd9e9fb225def5
pl12133 <krirken@gmail.com>
2015-11-27 02:22:17 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+(() => {
+  console.log('Runner runner');
+  const root = document.getElementById('root');
+
+  root.style.textAlign = 'center';
+  root.innerHTML = 'Go Code!';
+})();
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var host = process.env.HOST || 'localhost';
+var port = process.env.PORT || '8080';
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://'+host+':'+port,
+    './src/index'
+  ],
+  output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, '/dist/'),
+      publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json'],
+    modulesDirectories: ['node_modules', 'src'],
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
